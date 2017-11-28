b30471ca8fa9b07df902b1927e29e14ae9d1b3d5
haukur <haukur16@ru.is>
2017-11-28 12:34:36 +0000

    test
    test

---
+++ verkle/verkle/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  verkle
+//
+//  Created by Haukur Guðmundsson on 28/11/2017.
+//  Copyright © 2017 Haukur Guðmundsson. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    cout << "hello";
+    int number;
+    cin >> number;
+    cout << number+number;
+    
+    return 0;
+}
